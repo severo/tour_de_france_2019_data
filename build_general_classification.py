68926cc51ce88c8ce4d76ab611289a7fc658ec82
Sylvain Lesage <severo@rednegra.net>
2019-07-27 16:06:00 +0200

    feat: parse general classifiation directly
    Because there is no stage classification for stage 19...

---
+++ build_general_classification.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from parse_general_classification import parse_stage
+import os
+import json
+
+stages = []
+for stage_id in range(1,20):
+    html_file = os.path.join("saved_html", f"stage{stage_id}.html")
+    # open the file
+    with open(html_file, "r") as html:
+        stages.append(parse_stage(html))
+
+json_file = "general_classification.json"
+with open(json_file, "w") as f:
+    print(json.dumps(stages), file=f)
